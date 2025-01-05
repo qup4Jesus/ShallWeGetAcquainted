47fa9c31069ebeb05500ebaa2f3776c9eeef97ca
qupCharp <remret1654@gmail.com>
2025-01-05 18:49:12 +0700

    Задание 4.1.18

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -94,28 +94,59 @@ namespace ConsoleApp2
 
             string color = Console.ReadLine().ToLower();
 
-            if (color == "red" || color == "красный")
+            switch (color)
             {
-                Console.BackgroundColor = ConsoleColor.Red;
-                Console.ForegroundColor = ConsoleColor.Black;
+                case "красный":
+                    Console.BackgroundColor = ConsoleColor.Red;
+                    Console.ForegroundColor = ConsoleColor.Black;
 
-                Console.WriteLine("Твой цвет красный");
-            }
-            else if (color == "green" || color == "зеленый")
-            {
-                Console.BackgroundColor = ConsoleColor.Green;
-                Console.ForegroundColor = ConsoleColor.Blue;
+                    Console.WriteLine("Твой цвет красный!");
+                    break;
+                case "зеленый":
+                    Console.BackgroundColor = ConsoleColor.Green;
+                    Console.ForegroundColor = ConsoleColor.Blue;
 
-                Console.WriteLine("Твой цвет зеленый");
-            }
-            else
-            {
-                Console.BackgroundColor = ConsoleColor.Cyan;
-                Console.ForegroundColor = ConsoleColor.Magenta;
+                    Console.WriteLine("Твой цвет зеленый!");
+                    break;
+                case "голубой":
+                    Console.BackgroundColor = ConsoleColor.Cyan;
+                    Console.ForegroundColor = ConsoleColor.Magenta;
 
-                Console.WriteLine("Твой цвет голубой");
+                    Console.WriteLine("Твой цвет голубой!");
+                    break;
+                case "red":
+                    goto case "красный";
+                case "green":
+                    goto case "зеленый";
+                case "cyan":
+                    goto case "голубой";
+                default:
+                    Console.BackgroundColor = ConsoleColor.White;
+                    Console.ForegroundColor = ConsoleColor.Black;
+
+                    Console.WriteLine("Ты бетман!");
+                    Console.WriteLine("00000000000000000000000000000000000000000000000000\r\n" +
+                                      "0000000000000000000000_______000000000000000000000\r\n" +
+                                      "0000000000000_________________________000000000000\r\n" +
+                                      "0000000000____00______00__00_______00____000000000\r\n" +
+                                      "0000000____0000_______000000________0000____000000\r\n" +
+                                      "00000___000000________0000000_______0000000___0000\r\n" +
+                                      "0000__000000000______00000000_______000000000__000\r\n" +
+                                      "000_0000000000000___00000000000___000000000000___0\r\n" +
+                                      "00_00000000000000000000000000000000000000000000__0\r\n" +
+                                      "0__000000000000000000000000000000000000000000000_0\r\n" +
+                                      "0__000000000000000000000000000000000000000000000_0\r\n" +
+                                      "00_000000000000000000000000000000000000000000000_0\r\n" +
+                                      "00__0000000000000000000000000000000000000000000__0\r\n" +
+                                      "000__000000_____00____0000000____00_____000000__00\r\n" +
+                                      "0000___0000____________0000_____________0000___000\r\n" +
+                                      "000000___000____________00_____________000___00000\r\n" +
+                                      "00000000___________________________________0000000\r\n" +
+                                      "000000000000___________________________00000000000\r\n" +
+                                      "000000000000000000_______________00000000000000000\r\n" +
+                                      "00000000000000000000000000000000000000000000000000");
+                    break;
             }
         }
-
     }
 }
